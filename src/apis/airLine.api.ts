0121f7fd18fae0231f3d78e8a76a72c90115a947
Minh The <minhthe2804@gmail.com>
2025-11-20 22:21:16 +0700

    update

--- src/apis/airLine.api.ts
+++ src/apis/airLine.api.ts
@@ -52,7 +52,6 @@ export type AirlineFormData = {
     country_id: number // Cho phép undefined (khi chưa chọn)
     logo_url: string | null
     service_packages: ServicePackagePayload[] // mảng 'service_packages',
-    country_name?: string
 }
 // ------------------------------------
 
